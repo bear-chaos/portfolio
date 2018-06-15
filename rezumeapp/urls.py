9cd275926f53d22a739a2ebb3cbf2b4bb8dd5b67
Arthur Marshalko <eurek999@gmail.com>
2018-06-16 00:03:11 +0800

    Edit robots.txt

--- rezumeapp/urls.py
+++ rezumeapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import index, education, certificates, skills, my_projects, project_info
 
 app_name = 'rezumeapp'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('education/', education, name='education'),
     path('certificates/', certificates, name='certificates'),
     path('skills/', skills, name='skills'),
